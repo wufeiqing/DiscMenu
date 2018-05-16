5138db79edf8736f86169934c05c72380fd907e5
wufeiqing <543633269@qq.com>
2018-05-16 11:40:47 +0800

    添加点击注释

--- app/src/main/java/com/wufq/discmenu/MainActivity.java
+++ app/src/main/java/com/wufq/discmenu/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /**
      * 点击事件
-     * @param view
+     * @param view 视图
      */
     @Override
     public void onClick(View view) {
